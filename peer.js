c68e1d6b90d69da32752abcbb49ee0753c77390c
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-04-27 09:41:35 -0400

    try simpler scheme for connecting via DHT

--- peer.js
+++ peer.js
@@ -2,6 +2,7 @@
 var rudp = require('rudp')
 var EventEmitter = require('events').EventEmitter
 var inherits = require('util').inherits
+var debug = require('debug')('peer')
 var crypto = require('./crypto')
 var INTERVAL = 10000
 
@@ -9,11 +10,23 @@ function Peer(options) {
   EventEmitter.call(this)
   this.pub = options.pub
   this.priv = options.priv
+
+  var myPub = this.priv.getPublic(true, 'hex')
+  this.myInfoHash = crypto.toInfoHash(myPub)
+  this.peerInfoHash = crypto.toInfoHash(this.pub)
+  // this.me = {
+  //   forward: crypto.toInfoHash(myPub),
+  //   reverse: crypto.toRendezvousInfoHash(myPub)
+  // }
+
+  // this.peer = {
+  //   forward: crypto.toInfoHash(this.pub),
+  //   reverse: crypto.toRendezvousInfoHash(this.pub)
+  // }
+
   this.socket = options.socket
   this.port = this.socket.address().port
   this.dht = options.dht
-  this.infoHash = crypto.toInfoHash(this.pub)
-  this.rInfoHash = crypto.toRendezvousInfoHash(this.pub)
 
   if (this.dht.ready) this._watchDHT()
   else this.dht.once('ready', this._watchDHT.bind(this))
@@ -25,22 +38,46 @@ inherits(Peer, EventEmitter)
 
 Peer.prototype._watchDHT = function() {
   var self = this
+  var me = this.me
 
-  this._announcer = setInterval(function() {
-    self.dht.announce(self.infoHash, self.port)
-  }, INTERVAL)
+  ;(function announce() {
+    self._announcer = setTimeout(function() {
+      // debug('announcing', me.forward)
+      debug('announcing', self.myInfoHash)
+      self.dht.announce(self.myInfoHash, self.port, announce)
+      // self.dht.announce(me.forward, self.port, announce)
+      // self.dht.announce(peer.reverse, self.port)
+    }, INTERVAL)
+  })()
 
-  this._monitor = setInterval(function() {
-    self.dht.lookup(self.rInfoHash)
-  }, INTERVAL)
+  ;(function lookup() {
+    self._monitor = setTimeout(function() {
+      // debug('looking for peers for', me.reverse)
+      // self.dht.lookup(me.reverse, lookup)
+      debug('looking up', self.myInfoHash, self.peerInfoHash)
+      self.dht.lookup(self.myInfoHash, lookup)
+      self.dht.lookup(self.peerInfoHash)
+    }, INTERVAL)
+  })()
 
-  this.dht.on('peer:' + this.rInfoHash, function(addr, from) {
-    addr = addr.split(':')
-    self.connect(addr[0], addr[1])
+  this.dht.on('announce', function(addr, infoHash, from) {
+    if (infoHash === self.peerInfoHash) {
+      debug('got peer\'s announce', addr)
+      connect(addr)
+    }
   })
+
+  // this.dht.on('peer:' + me.reverse, connect)
+  this.dht.on('peer:' + this.myInfoHash, connect)
 
   this.ready = true
   this.emit('ready')
+
+  function connect(addr) {
+    debug('connecting to', addr)
+    addr = addr.split(':')
+    self.connect(addr[0], addr[1])
+  }
 }
 
 Peer.prototype.connect = function(host, port) {
